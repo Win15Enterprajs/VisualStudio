423d6f5bcfd68dd6059c4526ace50879786cc1ab
Drunkenwhisp <Kevinkarlberg@hotmail.se>
2016-02-23 10:25:45 +0100

    Added things

---
+++ VStest/VStest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VStest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // first line of code added
+            Console.WriteLine("Initial code added here");
+        }
+    }
+}
